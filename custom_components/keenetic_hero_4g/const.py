f40cef0f4631c9b63454ec9efa36a130b7fd407d
NikaSir <110602332+NikaSir@users.noreply.github.com>
2026-08-21 12:56:59 +0300

    Add integration constants

---
+++ custom_components/keenetic_hero_4g/const.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from datetime import timedelta
+
+DOMAIN = "keenetic_hero_4g"
+PLATFORMS = ["sensor", "binary_sensor"]
+
+DEFAULT_HOST = "192.168.0.1"
+DEFAULT_PORT = 80
+DEFAULT_SCAN_INTERVAL = 30
+DEFAULT_TIMEOUT = 10
+
+ETHERNET_INTERFACE = "GigabitEthernet1"
+LTE_INTERFACE = "UsbLte0"
+
+CONF_SCAN_INTERVAL = "scan_interval"
+UPDATE_INTERVAL = timedelta(seconds=DEFAULT_SCAN_INTERVAL)
